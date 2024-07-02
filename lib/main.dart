31a4f68503aa36ceef095e7a395ce2c7aa693545
PMarcelisROCMN <162563162+PMarcelisROCMN@users.noreply.github.com>
2024-07-02 09:29:18 +0200

    Business card project files

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    App(),
+  );
+}
+
+class App extends StatelessWidget {
+  const App({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return buildMaterialApp();
+  }
+
+  MaterialApp buildMaterialApp() {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 100, 147, 142),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 100,
+                  backgroundImage: AssetImage('images/rando1.jpeg'),
+                ),
+                InkWell(
+                  child: SizedBox(
+
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+
+                        ),
+                        child: Text(
+                          'button',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 40.0,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Pacifico'
+                          ),
+                        ),
+                      )
+                  ),
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.ad_units),
+        ),
+      ),
+    );
+  }
+}
