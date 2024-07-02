470febd08f2031a29b1c66271d89d7f89a423dfd
PMarcelisROCMN <162563162+PMarcelisROCMN@users.noreply.github.com>
2024-07-02 14:53:48 +0200

    Added basic design of the business card. Played around with inkwell for ontap event on the phone container.

--- lib/main.dart
+++ lib/main.dart
@@ -30,12 +30,101 @@ class App extends StatelessWidget {
                   backgroundImage: AssetImage('images/rando1.jpeg'),
                 ),
                 Text(
-                  'button',
+                  'Peter Marcelis',
                   style: TextStyle(
                       color: Colors.white,
                       fontSize: 40.0,
                       fontWeight: FontWeight.bold,
-                      fontFamily: 'Pacifico'
+                      fontFamily: 'Pacifico'),
+                ),
+                Text(
+                  'Professional clown'.toUpperCase(),
+                  style: TextStyle(
+                    color: Colors.teal.shade100,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    // fontStyle: FontStyle.italic,
+                    letterSpacing: 2,
+                    fontFamily: 'SourceSans',
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 96.0),
+                  child: Divider(
+                    color: Colors.cyan,
+                    height: 10,
+                    thickness: 1,
+                  ),
+                ),
+                Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: ClipRRect(
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(10),
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.phone_android,
+                              color: Colors.teal,
+                              size: 30.0,
+                            ),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Text(
+                              '+31 6 222 11 482',
+                              style: TextStyle(
+                                color: Colors.teal.shade200,
+                                fontFamily: 'SourceSans',
+                                letterSpacing: 2,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.email,
+                          color: Colors.teal,
+                          size: 30.0,
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text(
+                          'petermarcelis@gmail.com',
+                          style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontFamily: 'SourceSans',
+                            letterSpacing: 2,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+
+                          ),
+                          overflow: TextOverflow.fade,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
