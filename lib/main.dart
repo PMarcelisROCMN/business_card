e5c8a8cbabbf859cdd367ee2d398f98bc4fe68b6
PMarcelisROCMN <162563162+PMarcelisROCMN@users.noreply.github.com>
2024-07-03 09:22:19 +0200

    added openURL plugin (updated pubspec.yaml, etc) main.dart - added some styling and made the two rectangles into inkwell's with click functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+  final Uri _openCallMe = Uri.parse('tel: +31 6 222 11 482');
+  final Uri _openEmail = Uri.parse('mailto: petermarcelis95@gmail.com');
 
 void main() {
   runApp(
@@ -15,6 +19,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return buildMaterialApp();
   }
+
 
   MaterialApp buildMaterialApp() {
     return MaterialApp(
@@ -34,7 +39,7 @@ class App extends StatelessWidget {
                   style: TextStyle(
                       color: Colors.white,
                       fontSize: 40.0,
-                      fontWeight: FontWeight.bold,
+                      fontWeight: FontWeight.normal,
                       fontFamily: 'Pacifico'),
                 ),
                 Text(
@@ -51,46 +56,41 @@ class App extends StatelessWidget {
                 const Padding(
                   padding: EdgeInsets.symmetric(horizontal: 96.0),
                   child: Divider(
-                    color: Colors.cyan,
+                    color: Colors.teal,
                     height: 10,
-                    thickness: 1,
+                    thickness: 2,
                   ),
                 ),
                 Card(
                   margin:
                       EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                   color: Colors.white,
-                  shape: RoundedRectangleBorder(
+                  child: InkWell(
                     borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: ClipRRect(
-                    child: InkWell(
-                      borderRadius: BorderRadius.circular(10),
-                      onTap: () {},
-                      child: Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Row(
-                          children: <Widget>[
-                            Icon(
-                              Icons.phone_android,
-                              color: Colors.teal,
-                              size: 30.0,
+                    onTap: () => _launchUrl(_openCallMe),
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.phone_android,
+                            color: Colors.teal,
+                            size: 30.0,
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Text(
+                            '+31 6 222 11 482',
+                            style: TextStyle(
+                              color: Colors.teal.shade900,
+                              fontFamily: 'SourceSans',
+                              letterSpacing: 2,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20.0,
                             ),
-                            SizedBox(
-                              width: 20,
-                            ),
-                            Text(
-                              '+31 6 222 11 482',
-                              style: TextStyle(
-                                color: Colors.teal.shade200,
-                                fontFamily: 'SourceSans',
-                                letterSpacing: 2,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20.0,
-                              ),
-                            ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
@@ -99,31 +99,35 @@ class App extends StatelessWidget {
                   margin:
                       EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                   color: Colors.white,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Row(
-                      children: <Widget>[
-                        Icon(
-                          Icons.email,
-                          color: Colors.teal,
-                          size: 30.0,
-                        ),
-                        SizedBox(
-                          width: 20,
-                        ),
-                        Text(
-                          'petermarcelis@gmail.com',
-                          style: TextStyle(
-                            color: Colors.teal.shade900,
-                            fontFamily: 'SourceSans',
-                            letterSpacing: 2,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20.0,
-
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(10),
+                    onTap: () => _launchUrl(_openEmail),
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.email,
+                            color: Colors.teal,
+                            size: 30.0,
                           ),
-                          overflow: TextOverflow.fade,
-                        ),
-                      ],
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Text(
+                            'petermarcelis@gmail.com',
+                            style: TextStyle(
+                              color: Colors.teal.shade900,
+                              fontFamily: 'SourceSans',
+                              letterSpacing: 2,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20.0,
+
+                            ),
+                            overflow: TextOverflow.fade,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
@@ -137,5 +141,11 @@ class App extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+Future<void> _launchUrl(_url) async {
+  if (!await launchUrl(_url)) {
+    throw Exception('Could not launch $_url');
   }
 }
